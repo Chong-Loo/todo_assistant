b5d1cda1dae2b2c89449295d5a0b869b2401d986
luc31 <luc31@chinatelecom.cn>
2026-06-01 16:44:40 +0800

    add email password setting

--- scripts/set_password.py
+++ scripts/set_password.py
@@ -1,9 +1,23 @@
+"""
+手动设置密码的脚本
+"""
+
 import keyring
 
+# LLM Token
 keyring.set_password(
     "todo_assistant_llm",
     "default",
     ""
 )
-
 print("LLM Token 已保存")
+
+
+# # 邮箱密码
+# keyring.set_password(
+#     "todo_assistant",
+#     "default",
+#     ""
+# )
+
+# print("密码已保存")
